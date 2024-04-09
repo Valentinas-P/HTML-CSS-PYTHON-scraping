781f1636764952d80e2a3a9adf5c6ef0a8ec82ed
Valentinas <114878006+Valentinas-P@users.noreply.github.com>
2024-04-09 09:50:41 +0100

    Update tracking_monitor.py

--- Selenium Webdriver Browser and Game Playing Bot/tracking_monitor.py
+++ Selenium Webdriver Browser and Game Playing Bot/tracking_monitor.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import smtplib
 
-my_email = "pythonchr@gmail.com"
-password = "ural gtiy bosc kztc"
+my_email = "####################"
+password = "####################"
 
 
 URL = "https://www.amazon.co.uk/KOORUI-Inch-Computer-Monitor-AdaptiveSync/dp/B0B2RCXTK9/ref=sr_1_2_sspa?crid=3260IWO8BP2PK&dib=eyJ2IjoiMSJ9.KYgdzfdM9DBR5KeDyOlCN_R-rO27t13prVO0KXIbIXv184Z1O2dqd-PgVYrqkygllANToxPfb_ud2cDVBSHCgHBSN04EemAc1gd08UH5Ja4Hrzmu4yasDC61eTeZHti9TTsJYooAV4NVaSpD3NjzBuxAKyrqIsZ9w0bkcbovAQZ2bxAIWZvgOr7_yaYYmrUQ23Db4gip9iVAHN1P_E49qQFnV46Q2hmy0qDY-PCVxHo.c8kF2pdpxzhyebq0LmvPbp4KMwlWBxZM2M705BBVKQw&dib_tag=se&keywords=144hz%2Bmonitor&qid=1712061812&sprefix=144%2Caps%2C56&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
@@ -26,8 +26,8 @@ if float(price_currently) > looking_at_price:
         connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(
-            from_addr="pythonchr@gmail.com",
-            to_addrs="valiuncka@gmail.com",
+            from_addr="#################",
+            to_addrs="##################",
             msg=f"Subject: Amazon Price Tracker (Monitor)\n\nThe price went down based of the "
                 f"original price take a look at the price now it should be 99 pounds: {URL}"
         )
