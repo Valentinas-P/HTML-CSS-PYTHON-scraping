998de765bb8b131f0c7cbfbddea3cfa270d2c23c
Valentinas <114878006+Valentinas-P@users.noreply.github.com>
2024-04-13 12:28:37 +0100

    Update main.py

--- Web Scraping & Selenium - Zillow  House Search Automation/main.py
+++ Web Scraping & Selenium - Zillow  House Search Automation/main.py
@@ -38,21 +38,20 @@ class HouseSearch:
         for i in range(0, len(self.second_clean_section_by_symbol)):
             self.driver.get("https://docs.google.com/forms/d/e/1FAIpQLScMuydxX12B2mUzmQ5M27_quuVr9hlI0e5HArOWA73haDdfpA"
                             "/viewform?usp=sf_link")
-            address_field = self.driver.find_element(By.XPATH,
-                                                     value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+            address_field = self.driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
             time.sleep(1)
             address_field.send_keys(f"{self.addresses[i]}"), Keys.TAB
-            price_field = self.driver.find_element(By.XPATH,
-                                                   value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+            
+            price_field = self.driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
             price_field.send_keys(f"{self.second_clean_section_by_symbol[i]}"), Keys.TAB
-            link_field = self.driver.find_element(By.XPATH,
-                                                  value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+            
+            link_field = self.driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
             link_field.send_keys(f"{self.links_to_properties[i]}"), Keys.ENTER
-            submit_button = self.driver.find_element(By.XPATH,
-                                                     value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+            
+            submit_button = self.driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
             submit_button.click()
-            submit_another_response = self.driver.find_element(By.XPATH,
-                                                               value='/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+            
+            submit_another_response = self.driver.find_element(By.XPATH, value='/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
             submit_another_response.click()
 
 
